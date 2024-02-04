77ba643f777cba1ca04f8ad8530bbefec03842d6
lihongyao <lihy_online@163.com>
2024-02-04 15:33:02 +0800

    fix: 修复类型推断

--- useQuery.ts
+++ useQuery.ts
@@ -1,7 +1,7 @@
 /**
- * 读取 query 参数
- * @param key
- * @returns
+ * 读取 query
+ * @param key 
+ * @returns 
  */
 export default function useQuery<T = any>(key?: string) {
   const search = window.location.search;
@@ -9,18 +9,16 @@ export default function useQuery<T = any>(key?: string) {
     const queryString = search.slice(1);
     const queryParams: Record<string, string> = {};
 
-    queryString.split('&').forEach(pair => {
+    queryString.split('&').forEach((pair) => {
       const [k, v] = pair.split('=');
       queryParams[k] = v ? decodeURIComponent(v) : '';
     });
 
     if (key) {
-      return queryParams[key] as string;
+      return queryParams[key] as unknown as T;
     }
-
     return queryParams as unknown as T;
   }
-  return key ? '' : ({} as T);
+
+  return (key ? '' : {}) as unknown as T;
 }
-
-
